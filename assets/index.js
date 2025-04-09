e255cb24aea5823418c3eac7337e4338e4636524
Hiskiso <roma123qq@gmail.com>
2025-04-09 19:46:20 +0400

    финал

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,26 @@ new Glide('.glide', {
   startAt: 1,
   perView: 3,
   focusAt: 1,
-  classes:{
+  classes: {
     activeSlide: "activeSlide"
+  },
+  breakpoints:{
+    1080:{
+      perView: 2
+    },
+    830: {
+      perView: 1
+    }
   }
 }).mount()
+
+let openModalButton = document.getElementById("openModalButton")
+let closeModalButton = document.getElementById("closeModalButton")
+let modal = document.getElementById("modal")
+
+openModalButton.addEventListener("click", () => {
+  modal.style.display = "flex"
+})
+closeModalButton.addEventListener("click", () => {
+  modal.style.display = "none"
+}) 
